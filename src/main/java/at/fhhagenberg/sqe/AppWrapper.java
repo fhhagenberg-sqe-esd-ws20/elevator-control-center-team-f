58d28db7151aa1b36120bc5f8384238a2af0d338
Daniel G <giri@nwrk.biz>
2021-01-17 23:42:43 +0100

    Create UeberJAR for release (JAR file containing all dependencies. No fiddeling around at customer side).

---
+++ src/main/java/at/fhhagenberg/sqe/AppWrapper.java
@@ -0,0 +1,10 @@
+package at.fhhagenberg.sqe;
+
+/**
+ * Wrapper which allows creating an UEBERJAR which includes all dependencies.
+ */
+public class AppWrapper {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
